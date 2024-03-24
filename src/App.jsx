2a6030144992d50cb893b68fde92cb6439043ebe
Dibas Dauliya <dibasdauliya@gmail.com>
2024-03-23 20:15:46 -0400

    create home page

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+function App() {
+  return (
+    <div className='h-screen w-screen bg-hero-pattern bg-cover'>
+      <nav className='text-white max-w-7xl p-20 mx-auto'>
+        <div className='flex w-full justify-between'>
+          <strong className='text-2xl text-violet-500'>FitoFriend</strong>
+
+          <ul className='flex gap-3'>
+            <li className='underline font-semibold'>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </nav>
+
+      <main>
+        <h1 className='text-4xl font-bold text-white text-center pt-20'>
+          Your Interactive Fitness Friend!
+        </h1>
+        <p className='text-white text-xl text-center pt-5'>
+          Setup customized routine, track your workouts, get reminders, and
+          more!
+        </p>
+        <div className='flex justify-center pt-10'>
+          <button className='bg-violet-800 text-white p-4 rounded-lg'>
+            Get Started
+          </button>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
